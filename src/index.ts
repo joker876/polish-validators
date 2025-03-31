52a803b81cfe533fed9cebdee430268ac8896bb6
joker876 <joker876x@gmail.com>
2025-04-01 00:08:15 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+export * from './credit-card';
+export * from './doctor';
+export * from './iban';
+export * from './id-card';
+export * from './imei';
+export * from './isbn';
+export * from './nip';
 export * from './pesel';
+export * from './postal-code';
+export * from './regon';
 
